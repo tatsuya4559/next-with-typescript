10b00af9d7adb6dd4c09f1837474660c13e96e09
tatsuya4559 <tatsuya.k1029@gmail.com>
2020-04-22 07:43:43 +0900

    jest config

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  roots: ["<rootDir>/pages", "<rootDir>/components"],
+  testMatch: [
+    "**/__tests__/**/*.+(ts|tsx|js)",
+    "**/?(*.)+(spec|test).+(ts|tsx|js)",
+  ],
+  testPathIgnorePatterns: ["/node_modules/", "/.next/"],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+  transformIgnorePatterns: ["/node_modules/"],
+};
